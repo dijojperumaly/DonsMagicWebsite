9ee935f57d35f29d3d9443b4dbbfe2eb4ad4cbd9
dijojperumaly <dijoperumalil@gmail.com>
2024-03-11 13:08:41 +0530

    Saved on 11-3-24

---
+++ about.php
@@ -0,0 +1,111 @@
+<?php 
+include('headerWhite.php');
+?> 
+
+	<!-- Title page -->
+	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/bg-01.jpg');">
+		<h2 class="ltext-105 cl0 txt-center">
+			About
+		</h2>
+	</section>	
+
+
+	<!-- Content page -->
+	<section class="bg0 p-t-75 p-b-120">
+		<div class="container">
+			<div class="row p-b-148">
+				<div class="col-md-7 col-lg-8">
+					<div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
+						<h3 class="mtext-111 cl2 p-b-16">
+							Our Story
+						</h3>
+
+						<p class="stext-113 cl6 p-b-26">
+						
+						Transforming Threads into Dreams – Where Your Style Meets Our Craftsmanship.
+
+						</p>
+						<p class="stext-113 cl6 p-b-26">
+Where Every Stitch Weaves Your Dream – Tailoring Elegance Just for You.
+						</p>
+						<p class="stext-113 cl6 p-b-26">
+We are ready to weave your dreams
+						</p>
+						<p class="stext-113 cl6 p-b-26">
+No matter how you feel….get up…dress up…show up
+						</p>
+						<p class="stext-113 cl6 p-b-26">
+Designing…Manufacturing…Marketing…Distributing Garments 
+						</p>
+						<p class="stext-113 cl6 p-b-26">
+Specializing in comfort wear, we delve into the fashion realm of the everyday woman, offering a astonishing clothing collection for those who believe they cannot wear such styles.
+						</p>
+						<p class="stext-113 cl6 p-b-26">
+Empowering everyone through fashion, we believe that wearing the right dress enhances confidence. Experience this transformation affordably with our budget-friendly options.
+						</p>
+						<p class="stext-113 cl6 p-b-26">
+With our in-house stitching unit, bespoke designs, and punctual delivery, we ensure a seamless and tailored experience for our valued customers.
+						</p>
+						<p class="stext-113 cl6 p-b-26">
+Your comfort is our utmost priority. Catering to all ages, we offer a range of comfortable attire designed to embrace every stage of life.
+						</p>
+						<p class="stext-113 cl6 p-b-26">
+There's a certain magic woven that creates an unparalleled sense of comfort and ease…..DON’S MAGIC
+						</p>
+
+						<!-- <p class="stext-113 cl6 p-b-26">
+							Donec gravida lorem elit, quis condimentum ex semper sit amet. Fusce eget ligula magna. Aliquam aliquam imperdiet sodales. Ut fringilla turpis in vehicula vehicula. Pellentesque congue ac orci ut gravida. Aliquam erat volutpat. Donec iaculis lectus a arcu facilisis, eu sodales lectus sagittis. Etiam pellentesque, magna vel dictum rutrum, neque justo eleifend elit, vel tincidunt erat arcu ut sem. Sed rutrum, turpis ut commodo efficitur, quam velit convallis ipsum, et maximus enim ligula ac ligula. 
+						</p>
+
+						<p class="stext-113 cl6 p-b-26">
+							Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
+						</p> -->
+					</div>
+				</div>
+
+				<div class="col-11 col-md-5 col-lg-4 m-lr-auto">
+					<div class="how-bor1 ">
+						<div class="hov-img0">
+							<img src="images/about-01.jpg" alt="IMG">
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="order-md-2 col-md-7 col-lg-8 p-b-30">
+					<div class="p-t-7 p-l-85 p-l-15-lg p-l-0-md">
+						<h3 class="mtext-111 cl2 p-b-16">
+							Our Mission
+						</h3>
+
+						<p class="stext-113 cl6 p-b-26">
+							Mauris non lacinia magna. Sed nec lobortis dolor. Vestibulum rhoncus dignissim risus, sed consectetur erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam maximus mauris sit amet odio convallis, in pharetra magna gravida. Praesent sed nunc fermentum mi molestie tempor. Morbi vitae viverra odio. Pellentesque ac velit egestas, luctus arcu non, laoreet mauris. Sed in ipsum tempor, consequat odio in, porttitor ante. Ut mauris ligula, volutpat in sodales in, porta non odio. Pellentesque tempor urna vitae mi vestibulum, nec venenatis nulla lobortis. Proin at gravida ante. Mauris auctor purus at lacus maximus euismod. Pellentesque vulputate massa ut nisl hendrerit, eget elementum libero iaculis.
+						</p>
+
+						<div class="bor16 p-l-29 p-b-9 m-t-22">
+							<p class="stext-114 cl6 p-r-40 p-b-11">
+								Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn't really do it, they just saw something. It seemed obvious to them after a while.
+							</p>
+
+							<span class="stext-111 cl8">
+								- Steve Job’s 
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="order-md-1 col-11 col-md-5 col-lg-4 m-lr-auto p-b-30">
+					<div class="how-bor2">
+						<div class="hov-img0">
+							<img src="images/about-02.jpg" alt="IMG">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>	
+	
+<?php
+include('footer.php');
+?>
