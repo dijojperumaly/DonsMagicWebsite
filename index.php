24d53e72185cb03578dd2c225c2fac1b4b741ec0
dijojperumaly <dijoperumalil@gmail.com>
2024-04-05 12:11:13 +0530

    Changed Index page

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include('header.php');
 					<div class="block1 wrap-pic-w">
 						<img src="images/HomePage/cat1.png" alt="IMG-BANNER">
 
-						<a href="comfortAndHome.php" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+						<a href="ComfortAndHome.php" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 							<div class="block1-txt-child1 flex-col-l" >
 								<span class="block1-name ltext-102 trans-04 p-b-8" style="color: white;">
 									Home Wear
