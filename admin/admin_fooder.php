48312bd3b7099c221e90776df2a767c2199d76c9
Mr. Manoj Francis <CSSTF104@VJC.COM>
2024-08-07 13:05:22 +0530

    http links comment

--- admin/admin_fooder.php
+++ admin/admin_fooder.php
@@ -3,7 +3,7 @@
                 <footer class="footer">
             <div class="d-sm-flex justify-content-center justify-content-sm-between">
               <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © edendesigns.in 2024</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Designed by <a href="http://edendesigns.in/" target="_blank">Edendesigns</a></span>
+              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Designed by <a href="https://edendesigns.in/" target="_blank">Edendesigns</a></span>
             </div>
           </footer>
         </div>
